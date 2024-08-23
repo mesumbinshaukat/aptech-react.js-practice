a5e278cf63e9fd4363ad58f907f5a47c599a2684
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-08-23 13:32:34 +0500

    Updated

--- 02export_functions/src/App.jsx
+++ 02export_functions/src/App.jsx
@@ -2,27 +2,47 @@ import React from 'react'
 import Navbar from './Components/navbar'
 import Card from './Components/card'
 import Form from './Components/form'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
 
 export const App = () => {
   return (
     <>
-      <Navbar/>
-      <h1>This is App Page</h1>
-      <div className='row'>
-        <Card/>
-        <Card/>
-        <Card/>
-      </div>
-      <Form/>
-      
+    <BrowserRouter>
+      <Navbar></Navbar>
+
+        <Routes>
+          <Route path='/' element={<App/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }
 
-export const Home = () => {
-  return (
-    <>
-    <h3>This Is Home page</h3>
-    </>
-  )
-}
+
+// export const App = () => {
+//   return (
+//     <>
+//       <Navbar/>
+//       <h1>This is App Page</h1>
+//       <div className='row'>
+//         <Card/>
+//         <Card/>
+//         <Card/>
+//       </div>
+//       <Form/>
+      
+//     </>
+//   )
+// }
+
+// export const Home = () => {
+//   return (
+//     <>
+//     <h3>This Is Home page</h3>
+//     </>
+//   )
+// }
--- 02export_functions/src/main.jsx
+++ 02export_functions/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {App, Home} from './App.jsx'
+import {App} from './App.jsx'
+import About from './Pages/About.jsx'
+import Contact from './Pages/Contact.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<App/>}>
+    <Route path='/about' element={<About/>}/>
+    <Route path='contact-us' element={<Contact/>}/>
+  </Route>
+    
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Home />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
